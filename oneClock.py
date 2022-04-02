60ce3fc78805ddbdcd54f3458258bd5f8f72a286
woodchuck28 <charles.daab@gmail.com>
2022-04-02 07:43:45 -0400

    Modified functions for more ease of use

--- oneClock.py
+++ oneClock.py
@@ -8,11 +8,11 @@ def runOneClockModel():
     light = 300000000
 
     for i in range(5):
-        master_array = ship_journey(planet_distances_m[i], light)
+        master_array = ship_journey(planet_distances_m[i], light, 31536000000 ,.0000000000003)
         #Think of this as creating a huge matrix of all of the data so it is easy to export
         dataframe = pd.DataFrame(master_array)
 
         #this is simple but awesome, it takes our huge matrix and exports it to a csv datafile.
-        dataframe.to_csv(r"C:/Users/17703/Sandbox/PythonProjects/NinaProject/PlanetData/"+planets[i].name+"OneWay"+"data.csv")
+        dataframe.to_csv(r"C:/Users/17703/Sandbox/PythonProjects/NinaProject/PlanetData/BigDistances/"+planets[i].name+"OneWay"+"data.csv")
 
 
--- twoClock.py
+++ twoClock.py
@@ -9,11 +9,11 @@ def runTwoClockModel():
     #creating empty planets list
     
     for i in range(5):
-        master_array = ship_journey_twoWay(planet_distances_m[i], light)
+        master_array = ship_journey_twoWay(planet_distances_m[i], light, 31536000000 ,.0000000000003)
         #Think of this as creating a huge matrix of all of the data so it is easy to export
         dataframe = pd.DataFrame(master_array)
 
         #this is simple but awesome, it takes our huge matrix and exports it to a csv datafile.
-        dataframe.to_csv(r"C:/Users/17703/Sandbox/PythonProjects/NinaProject/PlanetData/"+planets[i].name+"TwoWay"+"data.csv")
+        dataframe.to_csv(r"C:/Users/17703/Sandbox/PythonProjects/NinaProject/PlanetData/BigDistances/"+planets[i].name+"TwoWay"+"data.csv")
 
 
